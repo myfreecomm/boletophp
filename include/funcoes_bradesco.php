dea2daebc361d179930f5c3d31009eb588ecb2e6
Rodrigo Cacilhas <rodrigo.cacilhas@myfreecomm.com.br>
2013-04-01 17:11:27 -0300

    no boleto para o Bradesco, se o dígito da agência estiver vazio, não coloca o tracinho

--- include/funcoes_bradesco.php
+++ include/funcoes_bradesco.php
@@ -61,7 +61,12 @@ $dv = digitoVerificador_barra("$codigobanco$nummoeda$fator_vencimento$valor$agen
 $linha = "$codigobanco$nummoeda$dv$fator_vencimento$valor$agencia$nnum$conta_cedente"."0";
 
 $nossonumero = substr($nnum,0,2).'/'.substr($nnum,2).'-'.$dv_nosso_numero;
-$agencia_codigo = $agencia."-".$dadosboleto["agencia_dv"]."/". $conta_cedente ."-". $conta_cedente_dv;
+
+$agencia_dv = $dadosboleto["agencia_dv"];
+if (strlen($agencia_dv) > 0) {
+	$agencia_dv = '-'.$agencia_dv;
+}
+$agencia_codigo = $agencia.$agencia_dv."/". $conta_cedente ."-". $conta_cedente_dv;
 
 
 $dadosboleto["codigo_barras"] = $linha;
